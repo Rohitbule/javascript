fe80ca94f43cd1564dac05648fc67ce6138ebef9
Rohitbule <rohitbule539@gmail.com>
2024-03-06 19:32:42 +0530

    day 1

---
+++ Day 1/app.js
@@ -0,0 +1,44 @@
+/*
+//printing statement
+    document.write("saniivani college of engineering");
+    console.log("Hello sanjivani");
+
+    let a=10;
+    let b=5;
+    document.write("sum is",a+b);
+    console.log("sum is",a+b);
+
+//template literals
+    let pencilsize=10;
+    let erasersize=5;
+    //let output="the total price is : "+(pencilsize + erasersize) + "Rupees.";
+    let output=`the total price is : ${pencilsize+erasersize} Rupees.`;
+    document.write(output);
+
+//operator
+    let a=10;
+    let b=5;
+    console.log(a+b);       //15
+    console.log(a++);       //10
+    console.log(++a);       //12
+    console.log(b=a);       //12
+    console.log(a==b);      //true
+    console.log(a>b);       //false
+    console.log(a!=b);      //false
+    console.log(a==='b');   //false
+    console.log(a===b);     //true
+    console.log("a"<"b");   //true
+*/
+
+//conditional statement
+let a=5;
+let b=6;
+if(a<b)
+{
+    console.log(a);
+}
+else
+{
+    console.log(b);
+}
+
