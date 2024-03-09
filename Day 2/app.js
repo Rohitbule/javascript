0c31a91f1173dfba44ce15e5892215d4dd3ab61e
Rohitbule <rohitbule539@gmail.com>
2024-03-09 23:36:50 +0530

    day 2

---
+++ Day 2/app.js
@@ -0,0 +1,19 @@
+//STRING METHODS=ACTION THAT CAN BE PERFORMED ON OBJECTS
+//FORMAT = STRINGNAME.METHOD()
+
+//str.trim=trims whitespace from both ends of strings & return a new one
+let password = prompt("set your password");
+let newpass=password.trim();
+console.log(newpass);
+
+//str.touppercase()="ROHIT"
+//str.tolowercase()="rohit"
+let name="ROHIT";
+let msg="bule";
+console.log(msg.toUpperCase());
+console.log(name.toLowerCase());
+
+//indexof()=returns the first index of occurence of some value in string.or gives -1 if not found
+let str="Ilovecoding";
+console.log(str.indexOf("love"));
+console.log(str.indexOf("o"));
