83337e3bfc9393de75941830ac4f37cebe9ae518
Rohitbule <rohitbule539@gmail.com>
2024-03-11 00:40:01 +0530

    string completed

--- Day 2/app.js
+++ Day 2/app.js
@@ -1,3 +1,4 @@
+
 //STRING METHODS=ACTION THAT CAN BE PERFORMED ON OBJECTS
 //FORMAT = STRINGNAME.METHOD()
 
@@ -19,8 +20,33 @@ console.log(str.indexOf("love"));
 console.log(str.indexOf("o"));
 
 //method chaining
-let a="  hello  "
-let b=msg.trim();
-console.log("after trim : ",b);
+let a="    sanjivani  "
+let b=a.trim();
+console.log("after trim :",b);
 b=b.toUpperCase();
 console.log("after uppercase : ",b);
+let c=a.trim().toUpperCase();   //method chaining
+console.log(c);
+
+//SLICE=returns a part of the original string as a new string
+let string="hello";
+console.log(string.slice(0,3));
+console.log(string.slice(0));
+console.log(string.slice(-2));  //5-2=3
+
+//REPLACE=Searches a value in the string & returns a new string with the value replaced
+console.log(string.replace("hello","i love coding"));
+console.log(string.replace("h","I"));
+
+//REPEAT=returns a string with the number of copies of a string.
+console.log(string.repeat(5));
+
+//PRACTICE PROBLEM
+let mssg="help";
+console.log(mssg.trim().toUpperCase());
+
+let namee="Apnacollege";
+console.log(namee.slice(4,9));
+console.log(namee.indexOf("na"));
+console.log(namee.replace("Apna","our"));
+console.log(namee.slice(4).replace("l","t").replace("l","t"));
