6ec95ecb42cec052e70bb03a22eaecbc0220f70d
Rohitbule <rohitbule539@gmail.com>
2024-03-18 23:12:29 +0530

    Day 3

---
+++ Day 3/app.js
@@ -0,0 +1,54 @@
+//Array = Linear collection of Array
+
+let marks=[99,85,93,76,62];
+let names=["adam","bob","catlyn"];
+let info=["aman",25,6.1]; //mixed array
+
+console.log(marks);
+console.log(names);
+console.log(info);
+console.log(typeof names);  //object
+console.log(info[0][0]);    //a
+console.log(info[0]);       //aman
+console.log(info.length);  //3
+console.log(info[0].length); //4
+
+//empty array
+let emparr=[];
+
+//Array are mutuable
+let name=["rohit","om","sai"];
+console.log(name);
+name[0]="tushar";
+console.log(name);
+name[10]="varad";
+console.log(name);
+
+// //Array methods
+// push:add to end
+// pop:delete from end and returns it.
+// unshift: add to Start
+// shift : delete from start and returns it.
+
+let car=["audi","bmw","xuv","maruti"];
+console.log(car);
+car.push("toyato");
+car.unshift("ferrari");
+console.log(car);
+car.pop("toyato");
+car.shift("ferrari");
+console.log(car);
+
+let follower=["a","b","c"];
+let block=follower.shift();
+console.log(follower);
+console.log(block);
+
+let start=["january","july","march","august"];
+console.log(start);
+start.shift();
+start.shift();
+console.log(start);
+start.unshift("june");
+start.unshift("july");
+console.log(start);
