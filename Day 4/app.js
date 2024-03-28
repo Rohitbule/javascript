5dcb92be921e29100fd82f08379892f870789e0e
Rohitbule <rohitbule539@gmail.com>
2024-03-29 00:14:43 +0530

    day 4

--- Day 4/app.js
+++ Day 4/app.js
@@ -92,14 +92,106 @@
 // console.log("we used break at 3");
 
 //LOOP WITH ARRAYS
-let fruits=['mango', 'apple', 'banana', 'litchi', 'orange']
-fruits.push("pineapple");                                   //it adding element to last of array
-for(let i=0; i<fruits.length; i++)
+// let fruits=['mango', 'apple', 'banana', 'litchi', 'orange']
+// fruits.push("pineapple");                                   //it adding element to last of array
+// for(let i=0; i<fruits.length; i++)
+// {
+//     console.log(i,'=', fruits[i])
+// }
+// console.log("reverse arry is printing");
+// for(let i=fruits.length-1; i>=0; i--)
+// {
+//     console.log(i, '=', fruits[i]);
+// }
+
+//NESTED LOOPS
+// let heroes = [ ['ironman', 'spiderman', 'thor'], ['superman', 'wonder woman', 'flash'] ]
+// for(let i=0; i<heroes.length; i++)
+// {
+//     console.log(i, heroes[i], heroes[i].length);
+//     for(let j=0; j<heroes[i].length; j++)
+//     {
+//         console.log(`j=${j}, ${heroes[i][j]}`);
+//     }
+// }
+
+// let student=[ ['aman',95], ['rohit',94.4], ['saiprasad',100]];
+// for(let i=0; i<student.length; i++)
+// {
+//     //console.log(`info of student #${i}' );
+//     console.log(`info of student #${i}`);
+
+//     for(let j=0; j<student[i].length; j++)
+//     {
+//         console.log(student[i][j]);
+//     }
+// }
+
+//FOR OF LOOP = WHEN WE HAVE AN COMPLEX COLLECTION 
+// let fruits=['mango', 'apple', 'banana', 'orange', 'litchi'];
+// for(i of fruits)
+// {
+//     console.log(i);
+// }
+
+// for( char of "apnacollege")
+// {
+//     console.log(char);
+// }
+
+//NESTED FOR OF LOOP
+// let hero = [ ['ironman', 'spiderman', 'thor'], ['superman', 'wonder woman', 'flash'] ];
+// for(list of hero )
+// {
+//     for(hero of list)
+//     {
+//         console.log(hero);
+//     }
+// }
+
+//TO DO APP
+//LIST - TO SHOW AL TODOS
+//ADD - TO ADD A TODO
+//DELETE - TO DELTE A TASK
+//QUIT - TO EXIT THE TODO
+
+let todo = [];
+let req = prompt("please enter your request");
+while(true)
 {
-    console.log(i,'=', fruits[i])
-}
-console.log("reverse arry is printing");
-for(let i=fruits.length-1; i>=0; i--)
-{
-    console.log(i, '=', fruits[i]);
+    if(req == 'quit')
+    {
+        console.log("quiting app");
+        break;
+    }
+
+    if(req == 'list')
+    {
+        console.log('------------------');
+        for(let i=0; i<todo.length; i++ )
+        {
+            console.log(i, todo[i]);
+        }
+        console.log('------------------');
+    }
+
+    else if(req == "add")
+    {
+        let task = prompt("please enter the task ypu want to add");
+        todo.push(task);
+        console.log("task added");
+    }
+
+    else if(req == "delete")
+    {
+        let idx = prompt("please enter the task index");
+        todo.splice(idx,1);
+        console.log("task deleted");
+    }
+
+    else{
+        console.log("wrong request");
+    }
+
+    req = prompt("please enter your request");
 }
