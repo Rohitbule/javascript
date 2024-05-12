55323824dd78353e89798b5efd0cc02d3a250e31
Rohitbule <rohitbule539@gmail.com>
2024-05-12 21:13:33 +0530

    day6

--- Day 6/app.js
+++ Day 6/app.js
@@ -98,10 +98,97 @@
 //scope : scope determine the accessiblity of variables,objects,and functions from different parts of the code.
 //function,block,lexical,global this are scope.
 //function scope: variables defined inside a function are not accessible(visible) from outside the function.
+//    let sum =54; //global scope
+//    function calsum(a,b)
+//    {
+//         //let sum = a+b;   //function scope
+//         console.log(sum);
+//    }
+//    calsum(1,2);
+//    console.log(sum);  
 
-function calsum(a,b)
+// Block Scope : variables declared inside a {} block cannot be accessed from outside the block .
+// let age =25;
+// if(age >=18)
+//     {
+//          let str = "adult";
+//          console.log(str);  //block chya atta madli value ch print kart
+//     }
+//console.log(str);  str blocked chya atta madhe declared kelel ahe.
+
+//Lexiacl Scope : a variable defined outside a function can be accesed inside another function defined after the variable declaration.
+//the oppsite is not true.
+
+// function outerfunction()
+// {
+//     let x = 10;
+//     let y = 20;
+//     function innerfunction()
+//     {
+//         //let a =200;
+//         console.log(x);
+//     }
+//     innerfunction();
+//    // console.log(a);
+// }
+// outerfunction();
+
+// let greet = "hello";
+
+// function changegreet()
+// {
+//     let greet = "namaste";
+//     console.log(greet);
+//     function innergreen()
+//     {
+//         console.log(greet);
+//     }
+// }
+// console.log(greet);
+// changegreet();
+
+//function expressions
+// let name = "Rohit";
+// let sum =  function(a,b)
+// {
+//     return a + b;
+// }
+// console.log(sum(1,2));
+
+//Higher order functions
+// a function that does one or both:
+//1. takes one or multiple functions functions as argument
+//2. returns a functions.
+
+//1
+function multiplegreet(func , n)
 {
-    let sum = a+b;
-    console.log(sum);
+    for(let i=1; i<=n; i++)
+        {
+            func();
+        }
 }
-calsum(1,2);
+
+let greet = function()
+{
+    console.log("hello");
+}
+multiplegreet(greet,2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
