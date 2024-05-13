afc64ce9592eb38e6f22d64e930ec3035328a711
Rohitbule <rohitbule539@gmail.com>
2024-05-13 18:30:16 +0530

    day 7

---
+++ Day 7/app.js
@@ -0,0 +1,81 @@
+//THIS KEYWORD : "THIS" keyword to an object that is executing the current piece of code .
+// const student  = {
+//     name : "shradhha",
+//     age : 23,
+//     eng : 95,
+//     math : 90,
+//     phy : 98,
+//     getavg()
+//     {
+//         console.log(this);
+//         let avg = (this.eng + this.math + this.phy) / 3;
+//         console.log(`${this.name} got avg marks = ${avg}`);
+//     }
+// }
+// //console.log(student.getavg());
+// function getavg()
+// {
+//     console.log(this);
+// }
+
+//try & catch
+//the ttry statement allows you to define a block of code to be tested for errors while it is being executed .
+//the catch statement allows you to define a block of code to be executed ,if an errors occurs in the try block.
+// console.log("hello");
+// console.log("hello");
+// //let a =10;
+// try
+// {
+//     console.log(a);
+// }
+// catch(err)
+// {
+//     console.log("Caught an error .. a is not defined");
+//     console.log(err);
+// }
+
+// console.log("hello2");
+// console.log("hello2");
+// console.log("hello2");
+
+//ARROW FUNCTIONS 
+// const sum = (a,b) =>
+// {
+//     console.log("the addition of two number is " ,a+b);
+// };
+// sum(2,3);
+
+// const cube = n => {
+//     return n*n*n;
+// }
+// console.log("the cube of number is",cube(3));
+
+// const pow = (a,b) =>
+//     {
+//         return a ** b;
+//     };
+// console.log("the power is ",pow(2,3));
+
+//Implict return 
+// const mul = (a,b) => (a*b);
+// console.log(mul(2,3));
+
+//SET TIMEOUT
+// console.log("Hi there!");
+// setTimeout(() => {
+//     console.log("Apna College");
+// },4000);
+// console.log("welcome to");
+
+//SET INTERVAL
+// let id = setInterval(() => {
+//     console.log("Apna College");
+// },2000);
+// console.log(id);
+// clearInterval(id);
+
+
+// let id2 = setInterval(() => {
+//     console.log("hi there");
+// },2000);
+// console.log(id2);
